1616f8d2cdc3542b070a41835519758faeb9b77a
Mykhailo Kholmohorov <mykhailo.kholmohorov@valtech.com>
2021-11-25 12:24:54 +0200

    add empty project and gitignore file

---
+++ Ankh-Morpork-game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ankh_Morpork_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Ankh-Morpork Game
