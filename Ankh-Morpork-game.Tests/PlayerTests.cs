f62c4f56263ff6d0cdd32e946ce68188b324cedb
Mykhailo Kholmohorov <mykhailo1257@gmail.com>
2021-11-29 02:06:26 +0200

    first test

--- Ankh-Morpork-game.Tests/PlayerTests.cs
+++ Ankh-Morpork-game.Tests/PlayerTests.cs
@@ -3,10 +3,10 @@ using NUnit.Framework;
 
 namespace Ankh_Morpork_game.Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [Test]
-        public void Test1()
+        [Test] public void GiveMoney_Payment20dollars_PlayerAmount80()
         {
             var player = new Player("Mike");
 
