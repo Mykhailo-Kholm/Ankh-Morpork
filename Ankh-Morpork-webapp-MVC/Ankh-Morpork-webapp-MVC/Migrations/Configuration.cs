784c3ec0fe81cceaba800b22987b825433ed55c5
Mykhailo Kholmohorov <mykhailo.kholmohorov@valtech.com>
2021-12-20 10:47:19 +0200

    fix

--- Ankh-Morpork-webapp-MVC/Ankh-Morpork-webapp-MVC/Migrations/Configuration.cs
+++ Ankh-Morpork-webapp-MVC/Ankh-Morpork-webapp-MVC/Migrations/Configuration.cs
@@ -35,22 +35,22 @@ namespace Ankh_Morpork_webapp_MVC.Migrations
 
             context.Beggars.AddRange(new List<Beggar>(11)
             {
-                new Beggar { Name = "Twitcher", Fee = 3, ImageUrl = @"1.jpg"},
-                new Beggar { Name = "Drooler", Fee = 2, ImageUrl = @"2.jpg"},
-                new Beggar { Name = "Dribbler", Fee = 1,ImageUrl = @"3.jpg"},
-                new Beggar { Name = "Mumbler", Fee = 1,ImageUrl = @"4.jpg"},
-                new Beggar { Name = "Mutterer", Fee = 0.9m,ImageUrl = @"5.jpg"},
-                new Beggar { Name = "Walking-Along-Shouters", Fee = 0.8m,ImageUrl = @"6.jpg"},
-                new Beggar { Name = "Demanders of a Chip", Fee = 0.6m,ImageUrl = @"7.jpg"},
-                new Beggar { Name = "People Who Call Other People Jimmy", Fee = 0.5m,ImageUrl = @"8.jpg"},
-                new Beggar { Name = "People Who Need Eightpence For A Meal", Fee = 0.08m,ImageUrl = @"9.jpg"},
-                new Beggar { Name = "People Who Need Tuppence For A Cup Of Tea", Fee = 0.02m,ImageUrl = @"10.jpg"},
-                new Beggar { Name = "People With Placards Saying \"Why lie? I need a beer.\"", Fee = 0,ImageUrl = @"11.jpg"},
+                new Beggar { Name = "Twitcher", Fee = 3, ImageUrl = @"1.png"},
+                new Beggar { Name = "Drooler", Fee = 2, ImageUrl = @"2.png"},
+                new Beggar { Name = "Dribbler", Fee = 1,ImageUrl = @"3.png"},
+                new Beggar { Name = "Mumbler", Fee = 1,ImageUrl = @"4.png"},
+                new Beggar { Name = "Mutterer", Fee = 0.9m,ImageUrl = @"5.png"},
+                new Beggar { Name = "Walking-Along-Shouters", Fee = 0.8m,ImageUrl = @"6.png"},
+                new Beggar { Name = "Demanders of a Chip", Fee = 0.6m,ImageUrl = @"7.png"},
+                new Beggar { Name = "People Who Call Other People Jimmy", Fee = 0.5m,ImageUrl = @"8.png"},
+                new Beggar { Name = "People Who Need Eightpence For A Meal", Fee = 0.08m,ImageUrl = @"9.png"},
+                new Beggar { Name = "People Who Need Tuppence For A Cup Of Tea", Fee = 0.02m,ImageUrl = @"10.png"},
+                new Beggar { Name = "People With Placards Saying \"Why lie? I need a beer.\"", Fee = 0,ImageUrl = @"11.png"},
             });
             context.Fools.AddRange(new List<Fool>(9)
             {
                 new Fool {Name = "Muggins", Fee = 0.5m, ImageUrl = @"1.png"},
-                new Fool {Name = "Gull", Fee = 1, ImageUrl = @"2png"},
+                new Fool {Name = "Gull", Fee = 1, ImageUrl = @"2.png"},
                 new Fool {Name = "Dupe", Fee = 2, ImageUrl = @"3.png"},
                 new Fool {Name = "Butt", Fee = 3, ImageUrl = @"4.png"},
                 new Fool {Name = "Fool", Fee = 5, ImageUrl = @"5.png"},
